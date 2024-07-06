238e2b955a97ce930dc7ba3d741eb50ba5791d31
donthampy1 <donthampyvalamkottil@gmail.com>
2024-07-06 19:27:45 +0530

    first commit

---
+++ chessboardlogic.js
@@ -0,0 +1,34 @@
+function generatechessboard() {
+   const maindiv =document.getElementById('maindiv');
+    maindiv.innerHTML='';
+
+    const rownumber= document.getElementById('rownumber').value;
+    const colnumber= document.getElementById('colnumber').value;
+    const oddcolor= document.getElementById('oddcolor').value;
+    const evencolor= document.getElementById('evencolor').value;
+
+    console.log(rownumber,colnumber,oddcolor,evencolor)
+
+    maindiv.style.gridTemplateRows=`repeat(${colnumber}, 50px)`;
+    maindiv.style.gridTemplateColumns =`repeat(${rownumber}, 50px)`;
+
+    for (i=0;i<rownumber;i++){
+        for(j=0;j<colnumber;j++){
+            const tile=document.createElement('div')
+            tile.classList.add('tile')
+
+            if ((i+j)%2===0) {
+                tile.style.backgroundColor = evencolor;
+                
+            } else {
+                tile.style.backgroundColor = oddcolor;
+            }
+            maindiv.appendChild(tile)
+
+
+
+        }
+    }
+   console.log('working')
+
+}
