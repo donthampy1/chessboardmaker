f397e3f1892430a610b886feb47e07cc738b5b59
donthampy1 <donthampyvalamkottil@gmail.com>
2024-07-07 23:23:23 +0530

    fixed roe issue

--- chessboardlogic.js
+++ chessboardlogic.js
@@ -9,8 +9,8 @@ function generatechessboard() {
 
     console.log(rownumber,colnumber,oddcolor,evencolor)
 
-    maindiv.style.gridTemplateRows=`repeat(${colnumber}, 50px)`;
-    maindiv.style.gridTemplateColumns =`repeat(${rownumber}, 50px)`;
+    maindiv.style.gridTemplateRows=`repeat(${rownumber}, 50px)`;
+    maindiv.style.gridTemplateColumns =`repeat(${colnumber}, 50px)`;
 
     for (i=0;i<rownumber;i++){
         for(j=0;j<colnumber;j++){
@@ -23,6 +23,7 @@ function generatechessboard() {
             } else {
                 tile.style.backgroundColor = oddcolor;
             }
+           console.log(i,j)
             maindiv.appendChild(tile)
 
 
